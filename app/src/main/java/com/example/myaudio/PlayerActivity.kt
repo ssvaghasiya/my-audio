45a50a9d18d7279133d1626d6b725ef0de49c2a1
ssvaghasiya <ssvaghasiya007@gmail.com>
2021-05-03 12:50:45 +0530

    scond commit

--- app/src/main/java/com/example/myaudio/PlayerActivity.kt
+++ app/src/main/java/com/example/myaudio/PlayerActivity.kt
@@ -1,17 +1,27 @@
 package com.example.myaudio
 
 import android.app.PendingIntent.getActivity
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.media.MediaMetadataRetriever
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_player.*
 
-class PlayerActivity : AppCompatActivity() {
+class PlayerActivity : AppCompatActivity(), MediaPlayer.OnCompletionListener {
 
     var position: Int = 0
     var handler: Handler = Handler()
@@ -31,7 +41,7 @@ class PlayerActivity : AppCompatActivity() {
         getIntentMethod()
         song_name.text = listSongs?.get(position)!!.title
         song_artist.text = listSongs?.get(position)!!.artist
-
+        mediaPlayer!!.setOnCompletionListener(this)
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 if (mediaPlayer != null && fromUser) {
@@ -93,7 +103,8 @@ class PlayerActivity : AppCompatActivity() {
                 song_artist.text = listSongs!!.get(position).artist
                 seekBar.max = mediaPlayer!!.duration / 1000
                 setProgressBar()
-                play_pause.setImageResource(R.drawable.ic_baseline_pause_24)
+                mediaPlayer!!.setOnCompletionListener(this)
+                play_pause.setBackgroundResource(R.drawable.ic_baseline_pause_24)
                 mediaPlayer!!.start()
 
             } else {
@@ -107,7 +118,8 @@ class PlayerActivity : AppCompatActivity() {
                 song_artist.text = listSongs!!.get(position).artist
                 seekBar.max = mediaPlayer!!.duration / 1000
                 setProgressBar()
-                play_pause.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+                mediaPlayer!!.setOnCompletionListener(this)
+                play_pause.setBackgroundResource(R.drawable.ic_baseline_play_arrow_24)
             }
         }
     }
@@ -136,7 +148,8 @@ class PlayerActivity : AppCompatActivity() {
                 song_artist.text = listSongs!!.get(position).artist
                 seekBar.max = mediaPlayer!!.duration / 1000
                 setProgressBar()
-                play_pause.setImageResource(R.drawable.ic_baseline_pause_24)
+                mediaPlayer!!.setOnCompletionListener(this)
+                play_pause.setBackgroundResource(R.drawable.ic_baseline_pause_24)
                 mediaPlayer!!.start()
 
             } else {
@@ -150,7 +163,8 @@ class PlayerActivity : AppCompatActivity() {
                 song_artist.text = listSongs!!.get(position).artist
                 seekBar.max = mediaPlayer!!.duration / 1000
                 setProgressBar()
-                play_pause.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+                mediaPlayer!!.setOnCompletionListener(this)
+                play_pause.setBackgroundResource(R.drawable.ic_baseline_play_arrow_24)
             }
         }
     }
@@ -215,16 +229,65 @@ class PlayerActivity : AppCompatActivity() {
         var durationtotal = listSongs?.get(position)?.duration!!.toInt() / 1000
         durationTotal.text = formattedTime(durationtotal)
         var art = retriever.embeddedPicture
+        var bitmap: Bitmap? = null
         if (art != null) {
-            Glide.with(this)
-                .asBitmap()
-                .load(art)
-                .into(cover_art)
+//            Glide.with(this)
+//                .asBitmap()
+//                .load(art)
+//                .into(cover_art)
+
+
+            bitmap = BitmapFactory.decodeByteArray(art, 0, art.size)
+            ImageAnimation(this, cover_art, bitmap!!)
+            Palette.from(bitmap).generate(object : Palette.PaletteAsyncListener {
+                override fun onGenerated(palette: Palette?) {
+                    var swatch: Palette.Swatch? = palette?.dominantSwatch
+                    if (swatch != null) {
+                        var gradient: ImageView = findViewById(R.id.imageViewGradient)
+                        var mContainer: RelativeLayout = findViewById(R.id.mContainer)
+                        gradient.setBackgroundResource(R.drawable.gradient_bg)
+                        mContainer.setBackgroundResource(R.drawable.main_bg)
+                        var myArray: IntArray = intArrayOf(swatch.rgb, 0x00000000)
+                        var gradientDrawable: GradientDrawable =
+                            GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, myArray)
+                        gradient.setBackground(gradientDrawable)
+                        var myArrayBG: IntArray = intArrayOf(swatch.rgb, swatch.rgb)
+                        var gradientDrawableBG: GradientDrawable =
+                            GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, myArrayBG)
+                        mContainer.setBackground(gradientDrawableBG)
+                        song_name.setTextColor(swatch.titleTextColor)
+                        song_artist.setTextColor(swatch.bodyTextColor)
+
+                    } else {
+                        var gradient: ImageView = findViewById(R.id.imageViewGradient)
+                        var mContainer: RelativeLayout = findViewById(R.id.mContainer)
+                        gradient.setBackgroundResource(R.drawable.gradient_bg)
+                        mContainer.setBackgroundResource(R.drawable.main_bg)
+                        var myArray: IntArray = intArrayOf(0xff000000.toInt(), 0x00000000)
+                        var gradientDrawable: GradientDrawable =
+                            GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, myArray)
+                        gradient.setBackground(gradientDrawable)
+                        var myArrayBG: IntArray = intArrayOf(0xff000000.toInt(), 0xff000000.toInt())
+                        var gradientDrawableBG: GradientDrawable =
+                            GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, myArrayBG)
+                        mContainer.setBackground(gradientDrawableBG)
+                        song_name.setTextColor(Color.WHITE)
+                        song_artist.setTextColor(Color.DKGRAY)
+                    }
+                }
+
+            })
         } else {
             Glide.with(this)
                 .asBitmap()
                 .load(R.drawable.ic_launcher_foreground)
                 .into(cover_art)
+            var gradient: ImageView = findViewById(R.id.imageViewGradient)
+            var mContainer: RelativeLayout = findViewById(R.id.mContainer)
+            gradient.setBackgroundResource(R.drawable.gradient_bg)
+            mContainer.setBackgroundResource(R.drawable.main_bg)
+            song_name.setTextColor(Color.WHITE)
+            song_artist.setTextColor(Color.DKGRAY)
         }
     }
 
@@ -246,6 +309,51 @@ class PlayerActivity : AppCompatActivity() {
             }
             seekBar.max = mediaPlayer!!.duration / 1000
             metaData(uri!!)
+        }
+    }
+
+    public fun ImageAnimation(context: Context, imageView: ImageView, bitmap: Bitmap) {
+
+        var animOut: Animation = AnimationUtils.loadAnimation(context, android.R.anim.fade_out)
+        var animIn: Animation = AnimationUtils.loadAnimation(context, android.R.anim.fade_in)
+        animOut.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                Glide.with(context).load(bitmap).into(imageView)
+                animIn.setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationRepeat(animation: Animation?) {
+
+                    }
+
+                    override fun onAnimationEnd(animation: Animation?) {
+
+                    }
+
+                    override fun onAnimationStart(animation: Animation?) {
+
+                    }
+
+                })
+                imageView.startAnimation(animIn)
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+        })
+        imageView.startAnimation(animOut)
+    }
+
+    override fun onCompletion(mp: MediaPlayer?) {
+        nextBtnClick()
+        if(mediaPlayer != null){
+            mediaPlayer = MediaPlayer.create(applicationContext,uri)
+            mediaPlayer!!.start()
+            mediaPlayer!!.setOnCompletionListener(this)
         }
     }
 }
